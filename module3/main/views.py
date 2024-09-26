3de707fa734e62b68ebd306812784daefe8f39a9
Istvan28 <160413679+Istvan28@users.noreply.github.com>
2024-09-26 11:45:05 +0200

    second

--- module3/main/views.py
+++ module3/main/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def index(request):
+    context = {
+        "title": "Home",
+        "content": "Internet bolt",
+        "list": ["first", "second"],
+        "dict": {"first": 1},
+        "is_authenticated": True,
+    }
+    return render(request, "main/index.html", context)
+
+
+def about(request):
+    return HttpResponse("About page")
