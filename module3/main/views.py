9aa04533dd1accefa383f40bfe55563bbe2299c0
Istvan28 <160413679+Istvan28@users.noreply.github.com>
2024-09-27 11:57:37 +0200

    edit urls

--- module3/main/views.py
+++ module3/main/views.py
@@ -7,14 +7,22 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        "title": "Home",
-        "content": "Internet bolt",
-        "list": ["first", "second"],
-        "dict": {"first": 1},
-        "is_authenticated": True,
+        "title": "Home - Fő oldal",
+        "content": "Магазин мебели HOME",
+        # "list": ["first", "second"],
+        # "dict": {"first": 1},
+        # "is_authenticated": True,
     }
     return render(request, "main/index.html", context)
 
 
 def about(request):
-    return HttpResponse("About page")
+    context = {
+        "title": "Home - Amit tudnod kell",
+        "content": "Магазин мебели HOME",
+        "text_on_page": "Amit tudnod kell",
+        # "list": ["first", "second"],
+        # "dict": {"first": 1},
+        # "is_authenticated": True,
+    }
+    return render(request, "main/about.html", context)
