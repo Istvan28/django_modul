a8143be5dd4542aa5cb648ed43afe471fb0fb619
Istvan28 <160413679+Istvan28@users.noreply.github.com>
2024-10-01 12:31:00 +0200

    fixtures

--- module3/main/views.py
+++ module3/main/views.py
@@ -1,14 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from goods.models import Categories
+
 
 # Create your views here.
 
 
 def index(request):
+    categories = Categories.objects.all()
+
     context = {
         "title": "Home - Fő oldal",
         "content": "Магазин мебели HOME",
+        "categories": categories,
         # "list": ["first", "second"],
         # "dict": {"first": 1},
         # "is_authenticated": True,
